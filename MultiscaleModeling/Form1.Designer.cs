9311dbbb54ee7d92908da760e35df8c2766d2727
Michał Słowikowski <slovik421@gmail.com>
2019-10-16 15:35:43 +0200

    Designed GUI

--- MultiscaleModeling/Form1.Designer.cs
+++ MultiscaleModeling/Form1.Designer.cs
@@ -65,6 +65,15 @@
             this.saveFileMenuItem = new System.Windows.Forms.MenuItem();
             this.jsonSaveFileMenuItem = new System.Windows.Forms.MenuItem();
             this.bitmapSaveFileMenuItem = new System.Windows.Forms.MenuItem();
+            this.menuTabControl = new System.Windows.Forms.TabControl();
+            this.propertiesTabPage = new System.Windows.Forms.TabPage();
+            this.executionTabPage = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tyoeInclusionsLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.typeInclusionsComboBox = new System.Windows.Forms.ComboBox();
+            this.valueInclusionsLabel = new System.Windows.Forms.Label();
+            this.valueInclusionsNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.viewPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.viewPictureBox)).BeginInit();
             this.viewGroupBox.SuspendLayout();
@@ -75,6 +84,11 @@
             this.caGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nucleonAmoutCAPropertiesNumericUpDown)).BeginInit();
             this.executionGroupBox.SuspendLayout();
+            this.menuTabControl.SuspendLayout();
+            this.propertiesTabPage.SuspendLayout();
+            this.executionTabPage.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valueInclusionsNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // viewPanel
@@ -104,7 +118,7 @@
             this.viewGroupBox.Controls.Add(this.viewGridCheckBox);
             this.viewGroupBox.Controls.Add(this.viewZoomTrackBarLabel);
             this.viewGroupBox.Controls.Add(this.viewZoomTrackBar);
-            this.viewGroupBox.Location = new System.Drawing.Point(525, 421);
+            this.viewGroupBox.Location = new System.Drawing.Point(4, 309);
             this.viewGroupBox.Name = "viewGroupBox";
             this.viewGroupBox.Size = new System.Drawing.Size(174, 91);
             this.viewGroupBox.TabIndex = 1;
@@ -152,7 +166,7 @@
             this.gridPropertiesGroupBox.Controls.Add(this.heightSizeGridPropertiesLabel);
             this.gridPropertiesGroupBox.Controls.Add(this.widthSizeGridPropertiesLabel);
             this.gridPropertiesGroupBox.Controls.Add(this.sizegGridPropertiesSLabel);
-            this.gridPropertiesGroupBox.Location = new System.Drawing.Point(525, 12);
+            this.gridPropertiesGroupBox.Location = new System.Drawing.Point(4, 6);
             this.gridPropertiesGroupBox.Name = "gridPropertiesGroupBox";
             this.gridPropertiesGroupBox.Size = new System.Drawing.Size(174, 117);
             this.gridPropertiesGroupBox.TabIndex = 2;
@@ -251,7 +265,7 @@
             this.caGroupBox.Controls.Add(this.boundaryConditionCAPropertiesComboBox);
             this.caGroupBox.Controls.Add(this.neighbourhoodCAPropertiesComboBoxLabel);
             this.caGroupBox.Controls.Add(this.neighbourhoodCAPropertiesComboBox);
-            this.caGroupBox.Location = new System.Drawing.Point(525, 135);
+            this.caGroupBox.Location = new System.Drawing.Point(4, 129);
             this.caGroupBox.Name = "caGroupBox";
             this.caGroupBox.Size = new System.Drawing.Size(174, 174);
             this.caGroupBox.TabIndex = 3;
@@ -376,7 +390,7 @@
             this.executionGroupBox.Controls.Add(this.caSimulationCAPropertiesLabel);
             this.executionGroupBox.Controls.Add(this.runCAExecutionButton);
             this.executionGroupBox.Controls.Add(this.stopCAExecutionButton);
-            this.executionGroupBox.Location = new System.Drawing.Point(525, 315);
+            this.executionGroupBox.Location = new System.Drawing.Point(4, 6);
             this.executionGroupBox.Name = "executionGroupBox";
             this.executionGroupBox.Size = new System.Drawing.Size(174, 100);
             this.executionGroupBox.TabIndex = 14;
@@ -443,15 +457,109 @@
             this.bitmapSaveFileMenuItem.Text = "... to BITMAP";
             this.bitmapSaveFileMenuItem.Click += new System.EventHandler(this.BitmapSaveFileMenuItem_Click);
             // 
+            // menuTabControl
+            // 
+            this.menuTabControl.Controls.Add(this.propertiesTabPage);
+            this.menuTabControl.Controls.Add(this.executionTabPage);
+            this.menuTabControl.Location = new System.Drawing.Point(515, 12);
+            this.menuTabControl.Name = "menuTabControl";
+            this.menuTabControl.SelectedIndex = 0;
+            this.menuTabControl.Size = new System.Drawing.Size(192, 500);
+            this.menuTabControl.TabIndex = 15;
+            // 
+            // propertiesTabPage
+            // 
+            this.propertiesTabPage.Controls.Add(this.gridPropertiesGroupBox);
+            this.propertiesTabPage.Controls.Add(this.caGroupBox);
+            this.propertiesTabPage.Controls.Add(this.viewGroupBox);
+            this.propertiesTabPage.Location = new System.Drawing.Point(4, 22);
+            this.propertiesTabPage.Name = "propertiesTabPage";
+            this.propertiesTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.propertiesTabPage.Size = new System.Drawing.Size(184, 474);
+            this.propertiesTabPage.TabIndex = 0;
+            this.propertiesTabPage.Text = "Properties";
+            this.propertiesTabPage.UseVisualStyleBackColor = true;
+            // 
+            // executionTabPage
+            // 
+            this.executionTabPage.Controls.Add(this.groupBox1);
+            this.executionTabPage.Controls.Add(this.executionGroupBox);
+            this.executionTabPage.Location = new System.Drawing.Point(4, 22);
+            this.executionTabPage.Name = "executionTabPage";
+            this.executionTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.executionTabPage.Size = new System.Drawing.Size(184, 474);
+            this.executionTabPage.TabIndex = 1;
+            this.executionTabPage.Text = "Execution";
+            this.executionTabPage.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.valueInclusionsNumericUpDown);
+            this.groupBox1.Controls.Add(this.valueInclusionsLabel);
+            this.groupBox1.Controls.Add(this.typeInclusionsComboBox);
+            this.groupBox1.Controls.Add(this.tyoeInclusionsLabel);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 112);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(174, 115);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inclusions";
+            // 
+            // tyoeInclusionsLabel
+            // 
+            this.tyoeInclusionsLabel.AutoSize = true;
+            this.tyoeInclusionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tyoeInclusionsLabel.Location = new System.Drawing.Point(24, 22);
+            this.tyoeInclusionsLabel.Name = "tyoeInclusionsLabel";
+            this.tyoeInclusionsLabel.Size = new System.Drawing.Size(34, 13);
+            this.tyoeInclusionsLabel.TabIndex = 11;
+            this.tyoeInclusionsLabel.Text = "Type:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // typeInclusionsComboBox
+            // 
+            this.typeInclusionsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.typeInclusionsComboBox.FormattingEnabled = true;
+            this.typeInclusionsComboBox.Items.AddRange(new object[] {
+            "Square",
+            "Circle"});
+            this.typeInclusionsComboBox.Location = new System.Drawing.Point(64, 19);
+            this.typeInclusionsComboBox.Name = "typeInclusionsComboBox";
+            this.typeInclusionsComboBox.Size = new System.Drawing.Size(104, 21);
+            this.typeInclusionsComboBox.TabIndex = 12;
+            // 
+            // valueInclusionsLabel
+            // 
+            this.valueInclusionsLabel.AutoSize = true;
+            this.valueInclusionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueInclusionsLabel.Location = new System.Drawing.Point(15, 59);
+            this.valueInclusionsLabel.Name = "valueInclusionsLabel";
+            this.valueInclusionsLabel.Size = new System.Drawing.Size(43, 13);
+            this.valueInclusionsLabel.TabIndex = 13;
+            this.valueInclusionsLabel.Text = "Length:";
+            // 
+            // valueInclusionsNumericUpDown
+            // 
+            this.valueInclusionsNumericUpDown.Location = new System.Drawing.Point(64, 57);
+            this.valueInclusionsNumericUpDown.Name = "valueInclusionsNumericUpDown";
+            this.valueInclusionsNumericUpDown.Size = new System.Drawing.Size(104, 20);
+            this.valueInclusionsNumericUpDown.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 522);
-            this.Controls.Add(this.executionGroupBox);
-            this.Controls.Add(this.caGroupBox);
-            this.Controls.Add(this.viewGroupBox);
-            this.Controls.Add(this.gridPropertiesGroupBox);
+            this.ClientSize = new System.Drawing.Size(709, 522);
+            this.Controls.Add(this.menuTabControl);
             this.Controls.Add(this.viewPanel);
             this.MaximizeBox = false;
             this.Menu = this.mainMenu1;
@@ -473,6 +581,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.nucleonAmoutCAPropertiesNumericUpDown)).EndInit();
             this.executionGroupBox.ResumeLayout(false);
             this.executionGroupBox.PerformLayout();
+            this.menuTabControl.ResumeLayout(false);
+            this.propertiesTabPage.ResumeLayout(false);
+            this.executionTabPage.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valueInclusionsNumericUpDown)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -515,6 +629,15 @@
         private System.Windows.Forms.MenuItem saveFileMenuItem;
         private System.Windows.Forms.MenuItem jsonSaveFileMenuItem;
         private System.Windows.Forms.MenuItem bitmapSaveFileMenuItem;
+        private System.Windows.Forms.TabControl menuTabControl;
+        private System.Windows.Forms.TabPage propertiesTabPage;
+        private System.Windows.Forms.TabPage executionTabPage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown valueInclusionsNumericUpDown;
+        private System.Windows.Forms.Label valueInclusionsLabel;
+        private System.Windows.Forms.ComboBox typeInclusionsComboBox;
+        private System.Windows.Forms.Label tyoeInclusionsLabel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
